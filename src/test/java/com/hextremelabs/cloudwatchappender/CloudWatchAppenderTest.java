96f4c90430910565bf0f9afd985cf3601ab0a2d0
Sayo Oladeji <oladejioluwasayo@gmail.com>
2018-01-20 11:34:27 +0200

    Predictable test ordering

--- src/test/java/com/hextremelabs/cloudwatchappender/CloudWatchAppenderTest.java
+++ src/test/java/com/hextremelabs/cloudwatchappender/CloudWatchAppenderTest.java
@@ -4,6 +4,7 @@ import org.apache.log4j.Category;
 import org.apache.log4j.Priority;
 import org.apache.log4j.spi.LoggingEvent;
 import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.Collection;
@@ -26,8 +27,26 @@ class CloudWatchAppenderTest {
 
   private static final ExecutorService POOL = Executors.newFixedThreadPool(100);
 
-  @Test()
-  void append() {
+  @Test
+  void retrieveLogsAndClear_emptyQueue() {
+    assertTrue(CloudWatchAppender.retrieveLogsAndClear().isEmpty());
+  }
+
+  @Test
+  void retrieveLogsAndClear_multiProducerPopulatedQueue() {
+    generate100kLogs();
+    final Collection<LoggingEvent> events = CloudWatchAppender.retrieveLogsAndClear();
+    assertEquals(100000, events.size());
+    final Iterator<LoggingEvent> iterator = events.iterator();
+    long previous = iterator.next().timeStamp;
+    while (iterator.hasNext()) {
+      long current = iterator.next().timeStamp;
+      assertTrue(current >= previous);
+      previous = current;
+    }
+  }
+
+  private static void generate100kLogs() {
     final List<Future> futures = new LinkedList<>();
     for (int a = 0; a < 100; a++) {
       futures.add(POOL.submit(newWritingTask()));
@@ -42,8 +61,6 @@ class CloudWatchAppenderTest {
         return false;
       }
     });
-
-    POOL.shutdown();
   }
 
   @NotNull
@@ -58,16 +75,8 @@ class CloudWatchAppenderTest {
     };
   }
 
-  @Test
-  void retrieveLogsAndClear() {
-    final Collection<LoggingEvent> events = CloudWatchAppender.retrieveLogsAndClear();
-    assertEquals(100000, events.size());
-    final Iterator<LoggingEvent> iterator = events.iterator();
-    long previous = iterator.next().timeStamp;
-    while (iterator.hasNext()) {
-      long current = iterator.next().timeStamp;
-      assertTrue(current >= previous);
-      previous = current;
-    }
+  @AfterAll
+  static void tearDown() {
+    POOL.shutdownNow();
   }
 }
